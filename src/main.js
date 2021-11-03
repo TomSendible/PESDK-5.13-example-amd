97fd72be0f12deb7176f02c0663b5f0872d39885
TomSendible <tom@sendible.com>
2021-11-03 15:19:58 +0000

    Update load order

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ requirejs.config({
   paths: {
     react: 'libs/react',
     "react-dom": 'libs/react-dom',
-    photoeditor: 'libs/PhotoEditor'
   },
   shim: {
     react: {
@@ -15,27 +14,22 @@ requirejs.config({
       deps: [],
       exports: ["react-dom"]
     },
-    photoeditor: {
-      deps: ["react", "react-dom"],
-      exports: "pesdk"
-    },
   }
 });
 
 define([
   "react",
   "react-dom",
-  "libs/PhotoEditor"
-], function ( _react, _reactDom, PhotoEditor,) {
- 
-  _reactDom.render(
-    _react.createElement(PhotoEditor.default, {
-      container: document.getElementById('photo__editor'),
-      editorInstance: () => {},
-      image: 'https://image.shutterstock.com/image-photo/word-example-written-on-magnifying-260nw-1883859943.jpg',
-      transform: {}
-    }),
-    document.getElementById('root')
-  );
-  
+], function ( _react, _reactDom,) {
+  require(["libs/PhotoEditor"], function(PhotoEditor){
+    _reactDom.render(
+      _react.createElement(PhotoEditor.default, {
+        container: document.getElementById('photo__editor'),
+        editorInstance: () => {},
+        image: 'https://image.shutterstock.com/image-photo/word-example-written-on-magnifying-260nw-1883859943.jpg',
+        transform: {}
+      }),
+      document.getElementById('root')
+    );
+   }) 
 });
